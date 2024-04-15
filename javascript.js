5c6bd745d015a3e3c440f943820dcd392e2db8f7
Rishabhpm23 <rxa6169@gmail.com>
2024-04-15 20:56:50 +0530

    edit css

--- javascript.js
+++ javascript.js
@@ -5,8 +5,6 @@ const gridBtn = document.getElementById('gridBtn');
     
             document.documentElement.style.setProperty('--size', squareNumber);
 
-            
-
             const gridContainer = document.getElementById('gridContainer');
             gridContainer.innerHTML = '';
              for (let i = 0; i < squareNumber * squareNumber; i++) {
@@ -14,11 +12,20 @@ const gridBtn = document.getElementById('gridBtn');
                 gridItem.classList.add('grid-item');
                 gridContainer.appendChild(gridItem);
                 if(gridItem){
-                    gridItem.addEventListener('mouseover', () => {
-                        gridItem.classList.add('my-colour');
+                    gridItem.addEventListener('click', () => {
+                        gridItem.classList.add('my-colour1');
                     });
-                }
+                } 
             } 
         })
 
     }
+    // const resetBtn = document.getElementById('resetBtn');
+    //     if(resetBtn){
+    //         resetBtn.addEventListener('click', () => {
+    //             for (let i = 0; i < squareNumber * squareNumber; i++) {
+    //                 document.getElementById('gridItem');
+    //                 gridItem.classList.toggle('my-colour2');
+    //             }
+    //         })
+    //     }
